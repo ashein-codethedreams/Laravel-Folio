b30c296ea24b4183eecd6207147f6fab2c407139
ashein-codethedreams <heroglasses155@gmail.com>
2024-03-28 10:37:20 +0630

    db table setup and data showing

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,28 @@
+@php
+    use App\Models\Blog;
+
+    $blogs = Blog::all();
+@endphp
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>My Blogs</h1>
+    <ul>
+        @foreach ($blogs as $blog)
+            <li>{{ $blog->title }} author - {{ $blog->author->name }}</li>
+        @endforeach
+
+    </ul>
+</body>
+
+</html>
